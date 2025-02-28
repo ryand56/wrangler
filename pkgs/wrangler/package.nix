ab6064cfe5e701cb359327a6bc12e44649681bfc
lemmih <lemmih@gmail.com>
2025-02-28 20:35:33 +0100

    skip upgrading wrangler

--- pkgs/wrangler/package.nix
+++ pkgs/wrangler/package.nix
@@ -19,9 +19,9 @@ let
   };
 
   pin = {
-    version = "3.111.0";
-    srcHash = "sha256-k15X2BrDeObRSF4gUKhyuv6F0+2HdoJx01J78R8Wxzc=";
-    pnpmDepsHash = "sha256-jzqQBApUeheu4v2fVQx8ewBPOi3fa47cKC+n2j6riUI=";
+    version = "3.107.2";
+    srcHash = "sha256-FIuilM1ZOFCYRnQtR3TKD2KyIDgzxvDiyoGX1RdmHnU=";
+    pnpmDepsHash = "sha256-NPbzBnbBKSnibHwPkEQDRxBr2XrABmCKLZH7vrydZq4=";
   };
 
   pname = "wrangler";
