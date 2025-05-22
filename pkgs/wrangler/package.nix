57e164265a5877b24a6f927a687a195ea68eebe0
ryand56 <22267679+ryand56@users.noreply.github.com>
2025-05-22 09:49:32 +0000

    wrangler: 3.114.8 -> 3.114.9

--- pkgs/wrangler/package.nix
+++ pkgs/wrangler/package.nix
@@ -19,9 +19,9 @@ let
   };
 
   pin = {
-    version = "3.114.8";
-    srcHash = "sha256-zsszv3dxScNMXTjiPiq55ucZzS7Tg46/if1854W1++E=";
-    pnpmDepsHash = "sha256-aqmJFabsqewB38Wmq3zKau5U61u+CTTk2Whpn5817xg=";
+    version = "3.114.9";
+    srcHash = "sha256-k3BEjWyGdwDZBw4Y3BXpJpVtB6B+IejAmYCLCR2ujcY=";
+    pnpmDepsHash = "sha256-Z055NyO5BdQXqq+lOCkokzgnur9GIsQXF1x/5230BhY=";
   };
 
   pname = "wrangler";
