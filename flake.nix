12c7ce0046fd598f25ce6f9140c4f67f48b9808c
Ryan Omasta <git@ryand.ca>
2025-05-22 03:58:04 -0600

    chore(flake): update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Wrangler, the CLI for Cloudflare Workers, packaged as a nix flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
